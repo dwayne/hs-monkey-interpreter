94a366a271be4d102950a135d8f64618244e5c53
Dwayne Crooks <me@dwaynecrooks.com>
2022-03-18 05:04:26 -0400

    Add a lexer

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,4 +1,62 @@
-module Lexer where
+module Lexer
+  ( identifier
+  , reserved
+  , symbol
+  , whiteSpace
+  )
+  where
 
 
--- WIP
+import qualified Data.Char as Char
+import qualified Text.Parsec as P
+import qualified Text.Parsec.Token as T
+
+import Control.Monad (mzero)
+import Text.Parsec.Language (LanguageDef, emptyDef)
+import Text.Parsec.String (Parser)
+import Text.Parsec.Token (TokenParser, makeTokenParser)
+
+
+identifier :: Parser String
+identifier = T.identifier monkey
+
+
+reserved :: String -> Parser ()
+reserved = T.reserved monkey
+
+
+symbol :: String -> Parser String
+symbol = T.symbol monkey
+
+
+whiteSpace :: Parser ()
+whiteSpace = T.whiteSpace monkey
+
+
+monkey :: TokenParser ()
+monkey = makeTokenParser monkeyDef
+
+
+monkeyDef :: LanguageDef ()
+monkeyDef =
+  emptyDef
+    { T.identStart = letter
+    , T.identLetter = letter
+    , T.opStart = mzero
+    , T.opLetter = mzero
+    , T.reservedNames =
+        [ "else"
+        , "false"
+        , "fn"
+        , "if"
+        , "let"
+        , "return"
+        , "true"
+        ]
+    }
+
+
+letter :: Parser Char
+letter = P.satisfy isLetter
+  where
+    isLetter c = Char.isAsciiLower c || Char.isAsciiUpper c || c == '_'
