af6c82afb990ba008debf81f905e422bf15d6cdc
Dwayne Crooks <me@dwaynecrooks.com>
2022-03-22 05:23:41 -0400

    The beginnings of an interpreter

---
+++ src/Interpreter.hs
@@ -0,0 +1,59 @@
+module Interpreter where
+
+
+import Parser
+import Text.Parsec (ParseError)
+
+
+data Value
+  = VNum Integer
+  | VNull
+  deriving (Show)
+
+
+data Error
+  = SyntaxError ParseError
+  deriving (Show)
+
+
+run :: String -> Either Error Value
+run input =
+  case parse input of
+    Right program ->
+      runProgram program
+
+    Left parseError ->
+      Left $ SyntaxError parseError
+
+
+runProgram :: Program -> Either Error Value
+runProgram (Program stmts) = runStmts stmts
+
+
+runStmts :: [Stmt] -> Either Error Value
+runStmts [] = Right VNull
+runStmts [stmt] = runStmt stmt
+runStmts (stmt : rest) = runStmt stmt >> runStmts rest
+
+
+runStmt :: Stmt -> Either Error Value
+runStmt stmt =
+  case stmt of
+    Let _ _ ->
+      Right VNull
+
+    Return _ ->
+      Right VNull
+
+    ExprStmt expr ->
+      runExpr expr
+
+
+runExpr :: Expr -> Either Error Value
+runExpr expr =
+  case expr of
+    Num n ->
+      Right $ VNum n
+
+    _ ->
+      Right VNull
